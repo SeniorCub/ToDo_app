69027ad0cfb88b84332ddbb8da530047d3bf693a
seniorcub <farinderif@gmail.com>
2024-11-12 17:14:48 +0100

    Add task management features and integrate CORS support

--- server/src/database.js
+++ server/src/database.js
@@ -8,10 +8,13 @@ const connect =  mysql.createPool({
      user: process.env.MYSQL_USER,
      password: process.env.MYSQL_PASSWORD,
      database: process.env.MYSQL_DATABASE,
+     socketPath: '/opt/lampp/var/mysql/mysql.sock'
 }).promise()
 
-const [result] = await connect.query('SELECT * FROM user_tb')
+const [users] = await connect.query('SELECT * FROM user_tb')
+const [tasks] = await connect.query('SELECT * FROM task_tb')
 
-console.log(result);
+console.log(users);
+console.log(tasks);
 
 export default connect
