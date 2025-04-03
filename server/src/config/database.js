b271a02102cefba4bc9f00cead423bb655ec3333
seniorcub <farinderif@gmail.com>
2025-04-03 23:04:12 +0100

    Refactor CORS configuration and enable database query logging

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -8,18 +8,18 @@ const connect = mysql.createPool({
      user: process.env.MYSQL_USER,
      password: process.env.MYSQL_PASSWORD,
      database: process.env.MYSQL_DATABASE,
-     socketPath: '/opt/lampp/var/mysql/mysql.sock'
+     // socketPath: '/opt/lampp/var/mysql/mysql.sock'
 }).promise()
  
 
-// const [users] = await connect.query('SELECT * FROM user_tb')
-// const [tasks] = await connect.query('SELECT * FROM task_tb')
-// const [notes] = await connect.query('SELECT * FROM note_tb')
-// const [diarys] = await connect.query('SELECT * FROM diary_tb')
+const [users] = await connect.query('SELECT * FROM user_tb')
+const [tasks] = await connect.query('SELECT * FROM task_tb')
+const [notes] = await connect.query('SELECT * FROM note_tb')
+const [diarys] = await connect.query('SELECT * FROM diary_tb')
 
-// console.log(users);
-// console.log(tasks);
-// console.log(notes);
-// console.log(diarys);
+console.log(users);
+console.log(tasks);
+console.log(notes);
+console.log(diarys);
 
 export default connect
