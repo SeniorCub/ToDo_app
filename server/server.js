a1340cd3e61163e549cbe6a4a062ba8b080434dd
seniorcub <farinderif@gmail.com>
2025-04-03 22:23:11 +0100

    Fixed cors

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const corsOptions = {
-     origin: 'http://localhost:5173',
+origin: 'https://weedtodo.vercel.app'
+	//     origin: 'http://localhost:5173',
      // origin: 'http://192.168.0.148:5173', // Fixed typo: removed the 'I' after IP
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
      allowedHeaders: ['Content-Type', 'Authorization'],
